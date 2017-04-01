00159dcd6ecc3db22a79255e1dceca35a12292cf
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-04-01 16:30:37 +0700

    adding prefereceUtils user preference

--- app/src/main/java/com/ezytopup/salesapp/utility/PreferenceUtils.java
+++ app/src/main/java/com/ezytopup/salesapp/utility/PreferenceUtils.java
@@ -15,22 +15,19 @@ public class PreferenceUtils {
     private final int STORENAME_KEY = R.string.settings_def_storename_key;
     private final int STORELOGO_KEY = R.string.settings_def_storelogo_key;
 
-    public static void setStoreDetail (Context context, String storeName, String logoUrl){
+    public static void setStoreDetail (Context context, String id, String first_name, String last_name,
+                                       String email, String phone_number, String access_token,
+                                       String image_user){
         SharedPreferences.Editor editor = Eztytopup.getsPreferences().edit();
-        editor.putString()
-
-        editor.putString(context.getString(R.string.settings_def_storename_key), storeName);
-        editor.putString(context.getString(R.string.settings_def_storelogo_key), logoUrl);
+        editor.putString(context.getString(R.string.settings_def_storeid_key), id);
+        editor.putString(context.getString(R.string.settings_def_storefirst_name_key), first_name);
+        editor.putString(context.getString(R.string.settings_def_storelast_name_key), last_name);
+        editor.putString(context.getString(R.string.settings_def_storeemail_key), email);
+        editor.putString(context.getString(R.string.settings_def_storephone_number_key), phone_number);
+        editor.putString(context.getString(R.string.settings_def_storeaccess_token_key), access_token);
+        editor.putString(context.getString(R.string.settings_def_storeimage_user_key), image_user);
         editor.apply();
     }
-
-    private int id;
-    private String first_name;
-    private String last_name;
-    private String email;
-    private String phone_number;
-    private String access_token;
-    private String image_user;
 
     public static String getSinglePrefrence (Context context, int prefereceKeyName){
         String result = null;
@@ -45,6 +42,43 @@ public class PreferenceUtils {
                         context.getString(R.string.settings_def_storelogo_key),
                         context.getString(R.string.settings_def_storelogo_default));
                 break;
+            case R.string.settings_def_storeid_key:
+                result = dataPreferece.getString(
+                        context.getString(R.string.settings_def_storeid_key),
+                        context.getString(R.string.settings_def_storeid_default));
+                break;
+            case R.string.settings_def_storefirst_name_key:
+                result = dataPreferece.getString(
+                        context.getString(R.string.settings_def_storefirst_name_key),
+                        context.getString(R.string.settings_def_storefirst_name_default));
+                break;
+
+            case R.string.settings_def_storelast_name_key:
+                result = dataPreferece.getString(
+                        context.getString(R.string.settings_def_storelast_name_key),
+                        context.getString(R.string.settings_def_storelast_name_default));
+                break;
+            case R.string.settings_def_storeemail_key:
+                result = dataPreferece.getString(
+                        context.getString(R.string.settings_def_storeemail_key),
+                        context.getString(R.string.settings_def_storeemail_default));
+                break;
+            case R.string.settings_def_storephone_number_key:
+                result = dataPreferece.getString(
+                        context.getString(R.string.settings_def_storephone_number_key),
+                        context.getString(R.string.settings_def_storephone_number_default));
+                break;
+            case R.string.settings_def_storeaccess_token_key:
+                result = dataPreferece.getString(
+                        context.getString(R.string.settings_def_storeaccess_token_key),
+                        context.getString(R.string.settings_def_storeaccess_token_default));
+                break;
+            case R.string.settings_def_storeimage_user_key:
+                result = dataPreferece.getString(
+                        context.getString(R.string.settings_def_storeimage_user_key),
+                        context.getString(R.string.settings_def_storeimage_user_default));
+                break;
+
         }
         return result;
     }
