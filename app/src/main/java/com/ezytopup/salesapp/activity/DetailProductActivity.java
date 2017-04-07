35431c2990a62fec0c088d145e5bee270127b96d
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-04-07 16:10:17 +0700

    add detailactivity layout on progress

---
+++ app/src/main/java/com/ezytopup/salesapp/activity/DetailProductActivity.java
@@ -0,0 +1,15 @@
+package com.ezytopup.salesapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ezytopup.salesapp.R;
+
+public class DetailProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_product);
+    }
+}
