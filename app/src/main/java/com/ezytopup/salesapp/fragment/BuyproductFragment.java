d18389da2aa9612a2064bd4b7c841d283f49f307
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-04-12 14:36:17 +0700

    sepate detail product and buy product

---
+++ app/src/main/java/com/ezytopup/salesapp/fragment/BuyproductFragment.java
@@ -0,0 +1,30 @@
+package com.ezytopup.salesapp.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ezytopup.salesapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BuyproductFragment extends Fragment {
+
+
+    public BuyproductFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_buyproduct, container, false);
+    }
+
+}
