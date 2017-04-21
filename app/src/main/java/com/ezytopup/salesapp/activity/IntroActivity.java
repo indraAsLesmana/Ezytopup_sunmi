fafa14b9ef2695b3a86b9fd18db36fae2b417a4d
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-04-21 17:36:10 +0700

    check is token still valid.

--- app/src/main/java/com/ezytopup/salesapp/activity/IntroActivity.java
+++ app/src/main/java/com/ezytopup/salesapp/activity/IntroActivity.java
@@ -1,65 +1,62 @@
 package com.ezytopup.salesapp.activity;
 
-import android.content.SharedPreferences;
-import android.os.AsyncTask;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-
 import com.ezytopup.salesapp.Eztytopup;
 import com.ezytopup.salesapp.R;
+import com.ezytopup.salesapp.api.TokencheckResponse;
 import com.ezytopup.salesapp.utility.PreferenceUtils;
+import java.net.HttpURLConnection;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class IntroActivity extends AppCompatActivity{
+    private static final String TAG = "IntroActivity";
     private View mProgressBar;
-    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-
         mProgressBar = findViewById(R.id.intro_progress_bar);
-        sharedPreferences = Eztytopup.getsPreferences();
-
-        new PreferenceCheck().execute(sharedPreferences);
         mProgressBar.setVisibility(View.VISIBLE);
-
+        String lastToken = PreferenceUtils.getSinglePrefrenceString(IntroActivity.this,
+                R.string.settings_def_storeaccess_token_key);
+        if (!lastToken.equals("-")){
+            tokenValidityCheck(lastToken);
+        }else {
+            PreferenceUtils.destroyUserSession(IntroActivity.this);
+            Login.start(IntroActivity.this);
+        }
     }
 
-    private class PreferenceCheck extends AsyncTask<SharedPreferences, Void, SharedPreferences>{
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        protected SharedPreferences doInBackground(SharedPreferences... params) {
-            SharedPreferences preferences = params[0];
-            return preferences;
-        }
-
-        @Override
-        protected void onPostExecute(SharedPreferences sharedPreferences) {
-            super.onPostExecute(sharedPreferences);
-            mProgressBar.setVisibility(View.GONE);
-
-            if (sharedPreferences.getAll() != null){
-                MainActivity.start(IntroActivity.this);
-            }else {
-                PreferenceUtils.destroyUserSession(IntroActivity.this);
-                Login.start(IntroActivity.this);
+    private void tokenValidityCheck(String token){
+        Call<TokencheckResponse> tokenResponse = Eztytopup.getsAPIService().checkToken(token);
+        tokenResponse.enqueue(new Callback<TokencheckResponse>() {
+            @Override
+            public void onResponse(Call<TokencheckResponse> call, Response<TokencheckResponse> response) {
+                if (response.isSuccessful()
+                        && response.body().status.getCode()
+                                .equals(String.valueOf(HttpURLConnection.HTTP_OK))
+                        && response.body().tokenValidity == Boolean.TRUE) {
+                    MainActivity.start(IntroActivity.this);
+                    finish();
+                } else {
+                    Log.i(TAG, String.format("onResponse: %s", response.body().status.getMessage()));
+                    PreferenceUtils.destroyUserSession(IntroActivity.this);
+                    Login.start(IntroActivity.this);
+                    finish();
+                }
             }
 
-        }
+            @Override
+            public void onFailure(Call<TokencheckResponse> call, Throwable t) {
+
+            }
+        });
     }
 
 }
