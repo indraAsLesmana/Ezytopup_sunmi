81148b8327037fef4515e627638923b09ebc5fab
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-04-25 15:40:46 +0700

    [Refactor] refactor Ezytopup API

--- app/src/main/java/com/ezytopup/salesapp/api/EzytopupAPI.java
+++ app/src/main/java/com/ezytopup/salesapp/api/EzytopupAPI.java
@@ -11,6 +11,8 @@ import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
+import static com.ezytopup.salesapp.utility.Constant.API_URL_GENERALUSAGE;
+
 /**
  * Created by indraaguslesmana on 3/31/17.
  */
@@ -28,60 +30,60 @@ public interface EzytopupAPI {
      * Live version
      * */
 
-    @POST("WGS_API_login.php?" + Constant.API_URL_GENERALUSAGE)
+    @POST("WGS_API_login.php?" + API_URL_GENERALUSAGE)
     Call<Authrequest> login_request (@Body Authrequest authrequest);
 
-    @GET("WGS_API_products.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_products.php?" + API_URL_GENERALUSAGE)
     Call<ProductResponse> getProduct();
 
-    @GET("WGS_API_getHeaderImages.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_getHeaderImages.php?" + API_URL_GENERALUSAGE)
     Call<HeaderimageResponse> getImageHeader();
 
-    @GET("WGS_API_best_seller_products.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_best_seller_products.php?" + API_URL_GENERALUSAGE)
     Call<BestSellerResponse> getBestSeller();
 
-    @GET("WGS_API_search_products.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_search_products.php?" + API_URL_GENERALUSAGE)
     Call<SearchResponse> getSearch(@Query("name") String productName);
 
-    @GET("WGS_API_get_detail_products.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_get_detail_products.php?" + API_URL_GENERALUSAGE)
     Call<DetailProductResponse> getDetailProduct(@Query("id") String productid);
 
-    @GET("WGS_API_panduan_awal.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_panduan_awal.php?" + API_URL_GENERALUSAGE)
     Call<TutorialResponse> getTutorial();
 
-    @GET("WGS_API_getFaq.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_getFaq.php?" + API_URL_GENERALUSAGE)
     Call<FaqResponse> getFaq();
 
-    @GET("WGS_API_getTerm.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_getTerm.php?" + API_URL_GENERALUSAGE)
     Call<TermResponse> getTerm();
 
-    @GET("WGS_API_categorized_products.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_categorized_products.php?" + API_URL_GENERALUSAGE)
     Call<CategoryResponse> getCategory(@Query("category_id") String productId);
 
-    @GET("WGS_API_payment_method.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_payment_method.php?" + API_URL_GENERALUSAGE)
     Call<PaymentResponse> getCheckactivePayment();
 
-    @GET("WGS_API_payment_method_internet_banking.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_payment_method_internet_banking.php?" + API_URL_GENERALUSAGE)
     Call<PaymentResponse> getPaymentInetBanking();
 
-    @GET("WGS_API_payment_method_bank_transfer.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_payment_method_bank_transfer.php?" + API_URL_GENERALUSAGE)
     Call<PaymentResponse> getPaymentBankTransfer();
 
-    @GET("WGS_API_payment_method_credit_card.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_payment_method_credit_card.php?" + API_URL_GENERALUSAGE)
     Call<PaymentResponse> getPaymentCreditcard();
 
-    @GET("WGS_API_payment_method_wallet.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_payment_method_wallet.php?" + API_URL_GENERALUSAGE)
     Call<PaymentResponse> getPaymentEzyWallet();
 
-    @GET("WGS_API_categories.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_categories.php?" + API_URL_GENERALUSAGE)
     Call<ListCategoryResponse> getListCategory();
 
-    @GET("WGS_API_get_order_transaction_history.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_get_order_transaction_history.php?" + API_URL_GENERALUSAGE)
     Call<TransactionHistoryResponse> getHistory(@Header("Authorize") String token, @Query("customerId") int customerId);
     /**
      * this API i implement with object PaymentResponse have no object data, just get Message for checking result 200 or not
      * */
-    @POST("WGS_API_abuyNow.php?" + Constant.API_URL_GENERALUSAGE)
+    @POST("WGS_API_abuyNow.php?" + API_URL_GENERALUSAGE)
     Call<PaymentResponse> buyNow(@Header("Authorize") String token,
                                  @Query("id") String id,
                                  @Query("session_name") String session_name,
@@ -105,18 +107,18 @@ public interface EzytopupAPI {
                                  @Query("templateCaption") String templateCaption,
                                  @Query("coupon_promo") String coupon_promo);
 
-    @GET("WGS_API_template_gift.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_template_gift.php?" + API_URL_GENERALUSAGE)
     Call<TamplateResponse> getTamplateGift();
 
-    @GET("WGS_API_verify_access_token.php?" + Constant.API_URL_GENERALUSAGE)
+    @GET("WGS_API_verify_access_token.php?" + API_URL_GENERALUSAGE)
     Call<TokencheckResponse> checkToken(@Header("Authorize") String token);
 
-    @POST("WGS_API_signup_tanpa_login.php?" + Constant.API_URL_GENERALUSAGE)
+    @POST("WGS_API_signup_tanpa_login.php?" + API_URL_GENERALUSAGE)
     Call<Authrequest> setLoginskip(@Query("provider") String provider,
                                     @Query("reg_gcm_id") String regfcmid,
                                     @Query("device_id") String deviceid);
 
-    @POST("WGS_API_login_reg_gcm_id.php?" + Constant.API_URL_GENERALUSAGE)
+    @POST("WGS_API_login_reg_gcm_id.php?" + API_URL_GENERALUSAGE)
     Call<Authrequest> setRegFcm(@Query("provider") String provider,
                                     @Query("reg_gcm_id") String regfcmid,
                                     @Query("device_id") String deviceid);
