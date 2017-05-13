6e060a75b352d923d870e9398cad9a2c321753b2
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-05-13 10:10:09 +0700

    [Add feature] add dinamicly make app as Laucher Home if device sunmi

--- app/src/main/java/com/ezytopup/salesapp/activity/IntroActivityLaucher.java
+++ app/src/main/java/com/ezytopup/salesapp/activity/IntroActivityLaucher.java
@@ -3,21 +3,23 @@ package com.ezytopup.salesapp.activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+
 import com.ezytopup.salesapp.Eztytopup;
 import com.ezytopup.salesapp.R;
 import com.ezytopup.salesapp.api.TokencheckResponse;
-import com.ezytopup.salesapp.utility.Constant;
 import com.ezytopup.salesapp.utility.PreferenceUtils;
+
 import java.net.HttpURLConnection;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class IntroActivity extends AppCompatActivity{
+public class IntroActivityLaucher extends AppCompatActivity{
     private static final String TAG = "IntroActivity";
     private View mProgressBar;
 
@@ -26,16 +28,9 @@ public class IntroActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
-        if (Eztytopup.getSunmiDevice()){
-            PackageManager p = IntroActivity.this.getPackageManager();
-            ComponentName cN = new ComponentName(IntroActivity.this, IntroActivityLaucher.class);
-            p.setComponentEnabledSetting(cN, PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-                    PackageManager.DONT_KILL_APP);
-        }
-
         mProgressBar = findViewById(R.id.intro_progress_bar);
         mProgressBar.setVisibility(View.VISIBLE);
-        String lastToken = PreferenceUtils.getSinglePrefrenceString(IntroActivity.this,
+        String lastToken = PreferenceUtils.getSinglePrefrenceString(IntroActivityLaucher.this,
                 R.string.settings_def_storeaccess_token_key);
 
       /*  if (!lastToken.equals(Constant.PREF_NULL)){
@@ -44,7 +39,7 @@ public class IntroActivity extends AppCompatActivity{
             PreferenceUtils.destroyUserSession(IntroActivity.this);
             Login.start(IntroActivity.this);
         }*/
-        Login.start(IntroActivity.this); //TODO : remove this after API running again.
+        Login.start(IntroActivityLaucher.this); //TODO : remove this after API running again.
     }
 
     private void tokenValidityCheck(String token){
@@ -54,14 +49,14 @@ public class IntroActivity extends AppCompatActivity{
             public void onResponse(Call<TokencheckResponse> call, Response<TokencheckResponse> response) {
                 if (response.isSuccessful()
                         && response.body().status.getCode()
-                        .equals(String.valueOf(HttpURLConnection.HTTP_OK))
+                                .equals(String.valueOf(HttpURLConnection.HTTP_OK))
                         && response.body().tokenValidity == Boolean.TRUE) {
-                    MainActivity.start(IntroActivity.this);
+                    MainActivity.start(IntroActivityLaucher.this);
                     finish();
                 } else {
                     Log.i(TAG, String.format("onResponse: %s", response.body().status.getMessage()));
-                    PreferenceUtils.destroyUserSession(IntroActivity.this);
-                    Login.start(IntroActivity.this);
+                    PreferenceUtils.destroyUserSession(IntroActivityLaucher.this);
+                    Login.start(IntroActivityLaucher.this);
                     finish();
                 }
             }
