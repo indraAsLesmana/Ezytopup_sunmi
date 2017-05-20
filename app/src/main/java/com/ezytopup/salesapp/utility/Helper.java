79a90ba290c6b9981137b701660386ca3b79c3ec
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-05-20 13:34:23 +0700

    remove DONE todo comment

--- app/src/main/java/com/ezytopup/salesapp/utility/Helper.java
+++ app/src/main/java/com/ezytopup/salesapp/utility/Helper.java
@@ -170,8 +170,6 @@ public class Helper {
         snackbar.show();
     }
 
-    // TODO : Download manager need permission on > Marshollow
-    // Unable to create directory: /storage/emulated/0/Ezytopup
     public static void downloadFile(Context context, String uRl) {
         File direct = new File(Environment.getExternalStorageDirectory()
                 + "/Ezytopup");
