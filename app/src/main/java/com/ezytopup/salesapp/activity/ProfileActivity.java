0fbcf84490ddef326a256a9461ec25640c81676f
Luthfan Maftuh <luthfanmaftuh@gmail.com>
2017-05-25 14:29:19 +0700

    Add getStringImage method to get the string of the image  from ImageFileSelector

--- app/src/main/java/com/ezytopup/salesapp/activity/ProfileActivity.java
+++ app/src/main/java/com/ezytopup/salesapp/activity/ProfileActivity.java
@@ -9,6 +9,7 @@ import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -30,6 +31,7 @@ import com.ezytopup.salesapp.utility.PreferenceUtils;
 import com.sw926.imagefileselector.ImageCropper;
 import com.sw926.imagefileselector.ImageFileSelector;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.net.HttpURLConnection;
 
@@ -268,5 +270,13 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
                 finish();
                 break;
         }
+    }
+
+    public String getStringImage(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
     }
 }
